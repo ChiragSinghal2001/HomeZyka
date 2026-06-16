cc6237aef8f7c414486cbbe815313329eb39dd5e
Chirag Singhal <chirag.singhal5088@gmail.com>
2026-06-16 10:17:50 +0530

    fixx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import Navbar from './Components/Navbar';
 import ProtectedRoute from './Components/ProtectedRoute';
